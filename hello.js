b77c79ab53c11342193af16504f11632a70c7b87
StellaGreen <vickysoler5@gmail.com>
2021-03-11 15:05:34 +0100

    entrainement

--- hello.js
+++ hello.js
@@ -1,3 +1,6 @@
+const chalk = require('chalk');
+const readlineSync = require('readline-sync');
+
 for (let i= 0; i < 10; i++){
-    console.log('hello word')
+    console.log(chalk.rgb(123 , 45 + i * 40, 67)('hello word'))
 }
